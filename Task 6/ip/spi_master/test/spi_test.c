23ca4b186e0ad92485446fecb74c8ca231c6ae06
urvikajadiya23 <urvikajadiya23@gmail.com>
2026-07-17 01:45:46 +0530

    Update spi_test.c

--- Task 6/ip/spi_master/test/spi_test.c
+++ Task 6/ip/spi_master/test/spi_test.c
@@ -1,12 +1,21 @@
 #include <stdint.h>
 
-#define LEDS      (*(volatile uint32_t *)0x400000)
-#define UART_DATA (*(volatile uint32_t *)0x400008)
-#define UART_CTRL (*(volatile uint32_t *)0x400010)
+#define LEDS        (*(volatile uint32_t *)0x400000)
+#define UART_DATA   (*(volatile uint32_t *)0x400008)
+#define UART_CTRL   (*(volatile uint32_t *)0x400010)
 
-void delay(volatile unsigned int d)
+/* Change this if your SPI base address is different */
+#define SPI_BASE    0x400040
+
+#define SPI_CTRL    (*(volatile uint32_t *)(SPI_BASE + 0x00))
+#define SPI_TXDATA  (*(volatile uint32_t *)(SPI_BASE + 0x04))
+#define SPI_RXDATA  (*(volatile uint32_t *)(SPI_BASE + 0x08))
+#define SPI_STATUS  (*(volatile uint32_t *)(SPI_BASE + 0x0C))
+
+void delay(volatile uint32_t d)
 {
-    while(d--);
+    while(d--)
+        asm volatile("nop");
 }
 
 void uart_putchar(char c)
@@ -15,27 +24,64 @@ void uart_putchar(char c)
     UART_DATA = c;
 }
 
-void uart_print(char *s)
+void uart_print(const char *s)
 {
     while(*s)
         uart_putchar(*s++);
 }
 
+void uart_print_hex8(uint8_t value)
+{
+    const char hex[]="0123456789ABCDEF";
+
+    uart_putchar('0');
+    uart_putchar('x');
+    uart_putchar(hex[(value>>4)&0xF]);
+    uart_putchar(hex[value&0xF]);
+}
+
 int main()
 {
-    uart_print("System Booted!\n");
+    uint8_t rx;
+
+    uart_print("\n===== SPI LOOPBACK TEST =====\n");
+
+    /* CLKDIV = 2, EN = 1 */
+    SPI_CTRL = (2<<8) | 0x01;
+
+    uart_print("Writing TXDATA = ");
+    uart_print_hex8(0xA5);
+    uart_print("\n");
+
+    SPI_TXDATA = 0xA5;
+
+    uart_print("Starting transfer...\n");
+
+    /* START = 1 */
+    SPI_CTRL = (2<<8) | 0x03;
+
+    /* wait DONE */
+    while((SPI_STATUS & 0x2)==0);
+
+    rx = SPI_RXDATA & 0xFF;
+
+    uart_print("Received RXDATA = ");
+    uart_print_hex8(rx);
+    uart_print("\n");
+
+    if(rx==0xA5)
+        uart_print("PASS\n");
+    else
+        uart_print("FAIL\n");
 
     while(1)
     {
         LEDS = 0x1F;
-        uart_print("LED ON\n");
         delay(500000);
 
         LEDS = 0x00;
-        uart_print("LED OFF\n");
         delay(500000);
     }
 
     return 0;
 }
-
