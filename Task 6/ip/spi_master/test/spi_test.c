e0a456b292f53bb933a88d4386f0dd62dc606532
urvikajadiya23 <urvikajadiya23@gmail.com>
2026-07-16 17:31:58 +0530

    Update spi_test.c

--- Task 6/ip/spi_master/test/spi_test.c
+++ Task 6/ip/spi_master/test/spi_test.c
@@ -1,53 +1,41 @@
 #include <stdint.h>
 
-#define IO_BASE 0x400000 
-#define UART_DATA  (*(volatile uint32_t *)(IO_BASE + 0x08))
-#define UART_CTRL  (*(volatile uint32_t *)(IO_BASE + 0x10))
-#define SPI_CTRL   (*(volatile uint32_t *)(0x400040))
-#define SPI_TXDATA (*(volatile uint32_t *)(0x400044))
-#define SPI_RXDATA (*(volatile uint32_t *)(0x400048))
-#define SPI_STATUS (*(volatile uint32_t *)(0x40004C))
+#define LEDS      (*(volatile uint32_t *)0x400000)
+#define UART_DATA (*(volatile uint32_t *)0x400008)
+#define UART_CTRL (*(volatile uint32_t *)0x400010)
+
+void delay(volatile unsigned int d)
+{
+    while(d--);
+}
 
 void uart_putchar(char c)
 {
-while(UART_CTRL & (1<<9));
-UART_DATA =c;
+    while(UART_CTRL & (1<<9));
+    UART_DATA = c;
 }
 
 void uart_print(char *s)
 {
-while(*s)
-uart_putchar(*s++);
-}
-
-void uart_print_hex(uint32_t x)
-{
-char hex[]="0123456789ABCDEF";
-
-uart_print("0x");
-
-for(int i=28;i>=0;i-=4)
-uart_putchar(hex[(x>>i)&0xF]);
-
-uart_putchar('\n');
+    while(*s)
+        uart_putchar(*s++);
 }
 
 int main()
 {
-uart_print ("SPI TEST START  ");
-SPI_CTRL = (1<<0)|(2<<8);
-SPI_TXDATA = 0xA5;
-SPI_CTRL|= (1<<1);
+    uart_print("System Booted!\n");
 
-while(!(SPI_STATUS & (1<<1)));
+    while(1)
+    {
+        LEDS = 0x1F;
+        uart_print("LED ON\n");
+        delay(500000);
 
-uart_print("RX = ");
-uart_print_hex(SPI_RXDATA);
+        LEDS = 0x00;
+        uart_print("LED OFF\n");
+        delay(500000);
+    }
 
-uart_print("SPI TEST DONE  ");
-
-while(1);
-
-return 0;
-
+    return 0;
 }
+
